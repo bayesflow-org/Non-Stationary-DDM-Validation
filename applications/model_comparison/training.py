73e9f55c7fd8598350785c6cd1f2b68e7960933b
Lukas Schumacher <39727780+LuSchumacher@users.noreply.github.com>
2023-08-04 09:22:21 +0200

    add training.py

---
+++ applications/model_comparison/training.py
@@ -0,0 +1,31 @@
+# Get rid of annoying tf warning
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
+import pickle
+import numpy as np
+np.set_printoptions(suppress=True)
+
+import bayesflow as beef
+import tensorflow as tf
+
+import sys
+sys.path.append("../")
+from experiment import ModelComparisonExperiment
+
+# gpu setting and checking
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
+print(tf.config.list_physical_devices('GPU'))
+
+experiment = ModelComparisonExperiment()
+
+with open('../data/training_data.pkl', 'rb') as f:
+    training_data = pickle.load(f)
+with open('../data/validation_data.pkl', 'rb') as f:
+    validation_data = pickle.load(f)
+
+history = experiment.run(
+    training_data=training_data,
+    validation_data=None
+    )
