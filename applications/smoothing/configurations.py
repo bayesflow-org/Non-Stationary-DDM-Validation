b021dd7a63bef4340b19668f871901b689d1ded3
Lukas Schumacher <39727780+LuSchumacher@users.noreply.github.com>
2023-08-22 08:53:30 +0200

    add smoothing experiment

---
+++ applications/smoothing/configurations.py
@@ -0,0 +1,34 @@
+default_settings = {
+    "lstm1_hidden_units": 512,
+    "lstm2_hidden_units": 256,
+    "lstm3_hidden_units": 128,
+    "local_amortizer_settings": {
+        "num_coupling_layers": 8,
+        "coupling_design": 'interleaved'
+    },
+    "global_amortizer_settings": {
+        "num_coupling_layers": 6,
+        "coupling_design": 'interleaved'
+    },
+    "trainer": {
+        "max_to_keep": 1,
+        "default_lr": 5e-4,
+        "memory": False,
+    },
+}
+default_bounds = {
+    "lower": (0.0, 0.2, 0.0),
+    "upper": (8.0, 6.0, 4.0)
+}
+default_priors = {
+    # v, a, tau
+    "ddm_loc": (2.0, 2.0, 0.3),
+    "ddm_scale": (2.0, 1.5, 1.0),
+    "scale_loc": (0.0, 0.0, 0.0),
+    "scale_scale": (0.1, 0.1, 0.01),
+    # v, a
+    "q_low": (0.0, 0.0),
+    "q_high": (0.2, 0.1),
+    "alpha_a": (1.5, 2.5),
+    "alpha_b": (1.5, 1.5)
+}
---
+++ applications/smoothing/experiments.py
@@ -0,0 +1,162 @@
+from abc import ABC, abstractmethod
+import bayesflow as bf
+import tensorflow as tf
+
+from tensorflow.keras.layers import LSTM, Bidirectional
+from tensorflow.keras.models import Sequential
+
+from configurations import default_settings
+
+class Experiment(ABC):
+    """An interface for a standardized experiment."""
+
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def run(self, epochs=75, iterations_per_epoch=1000, batch_size=16):
+        """Wrapper for online training
+
+        Parameters:
+        -----------
+        epochs: int, optional, default: 75
+            Number of trainig epochs.
+        iterations_per_epoch, int, optional, default: 1000
+            Number of iterations per epoch.
+        batch_size: int, optional, default: 16
+            Number of simulated data sets per batch.
+
+        Returns:
+        --------
+        history : dict
+            A dictionary with the training history
+        """
+
+        history = self.trainer.train_online(epochs, iterations_per_epoch, batch_size)
+        return history
+
+
+class SmoothingExperiment(Experiment):
+    """Wrapper for estimating a non-stationary diffusion decision model
+    with a smoothing approach"""
+
+    def __init__(self, model, checkpoint_path=None, config=default_settings):
+        """Creates an instance of the model with given configuration. When used in a BayesFlow pipeline,
+        only the attribute ``self.generator`` and the method ``self.configure`` should be used.
+
+        Parameters:
+        -----------
+        model   : an instance of models.RandomWalkDiffusion
+            The model wrapper, should include a callable attribute ``generator`` and a method
+            ``configure()``
+        config  : dict, optional, default: ``configuration.default_settings``
+            A configuration dictionary with the following keys:
+            ``lstm1_hidden_units``        - The dimensions of the first LSTM of the first summary net
+            ``lstm2_hidden_units``        - The dimensions of the second LSTM of the first summary net
+            ``lstm3_hidden_units``        - The dimensions of the third LSTM of the second summary net
+            ``trainer``                   - The settings for the ``bf.trainers.Trainer``, not icnluding   
+                the ``amortizer``, ``generative_model``, and ``configurator`` keys,
+                as these will be provided internaly by the Experiment instance
+        """
+
+        self.model = model
+
+        # Smoothing network
+        self.summary_network = bf.networks.HierarchicalNetwork([
+            Sequential([
+                Bidirectional(LSTM(config["lstm1_hidden_units"], return_sequences=True)),
+                Bidirectional(LSTM(config["lstm2_hidden_units"], return_sequences=True)),
+            ]),
+            Sequential([Bidirectional(LSTM(config["lstm3_hidden_units"]))])
+        ])
+
+        self.local_net = bf.amortizers.AmortizedPosterior(
+            bf.networks.InvertibleNetwork(
+                num_params=3,
+                **config.get("local_amortizer_settings")
+            ))
+
+        self.global_net = bf.amortizers.AmortizedPosterior(
+            bf.networks.InvertibleNetwork(
+                num_params=model.hyper_prior_mean.shape[0],
+                **config.get("global_amortizer_settings")
+            ))
+
+        self.amortizer = bf.amortizers.TwoLevelAmortizedPosterior(
+            self.local_net,
+            self.global_net,
+            self.summary_network
+            )
+
+        # Trainer setup
+        self.trainer = bf.trainers.Trainer(
+            amortizer=self.amortizer,
+            generative_model=self.model.generate,
+            configurator=self.model.configure,
+            checkpoint_path=checkpoint_path,
+            **config.get("trainer")
+        )
+
+
+class FilteringExperiment(Experiment):
+    """Wrapper for estimating a non-stationary diffusion decision model
+    with a filtering approach"""
+
+    def __init__(self, model, checkpoint_path=None, config=default_settings):
+        """Creates an instance of the model with given configuration. When used in a BayesFlow pipeline,
+        only the attribute ``self.generator`` and the method ``self.configure`` should be used.
+
+        Parameters:
+        -----------
+        model   : an instance of models.RandomWalkDiffusion
+            The model wrapper, should include a callable attribute ``generator`` and a method
+            ``configure()``
+        config  : dict, optional, default: ``configuration.default_settings``
+            A configuration dictionary with the following keys:
+            ``lstm1_hidden_units``        - The dimensions of the first LSTM of the first summary net
+            ``lstm2_hidden_units``        - The dimensions of the second LSTM of the first summary net
+            ``lstm3_hidden_units``        - The dimensions of the third LSTM of the second summary net
+            ``trainer``                   - The settings for the ``bf.trainers.Trainer``, not icnluding   
+                the ``amortizer``, ``generative_model``, and ``configurator`` keys,
+                as these will be provided internaly by the Experiment instance
+        """
+
+        self.model = model
+
+        # Filtering summary network
+        self.summary_network = bf.networks.HierarchicalNetwork([
+            Sequential([
+                LSTM(config["lstm1_hidden_units"], return_sequences=True),
+                LSTM(config["lstm2_hidden_units"], return_sequences=True),
+            ]),
+            Sequential([LSTM(config["lstm3_hidden_units"])])
+        ])
+
+        self.local_net = bf.amortizers.AmortizedPosterior(
+            bf.networks.InvertibleNetwork(
+                num_params=3,
+                **config.get("local_amortizer_settings")
+            ))
+
+        self.global_net = bf.amortizers.AmortizedPosterior(
+            bf.networks.InvertibleNetwork(
+                num_params=model.hyper_prior_mean.shape[0],
+                **config.get("global_amortizer_settings")
+            ))
+
+        self.amortizer = bf.amortizers.TwoLevelAmortizedPosterior(
+            self.local_net,
+            self.global_net,
+            self.summary_network
+            )
+
+        # Trainer setup
+        self.trainer = bf.trainers.Trainer(
+            amortizer=self.amortizer,
+            generative_model=self.model.generate,
+            configurator=self.model.configure,
+            checkpoint_path=checkpoint_path,
+            **config.get("trainer")
+        )
+
